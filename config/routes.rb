3434d9f75c54d9d5f79e4fe2dd1a0236b93a22d4
TakahiroT0415156 <teradatakahiro16@gmail.com>
2022-06-04 17:09:55 +0900

    コントローラーの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
-  root 'posts#new'
+  root 'posts#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
