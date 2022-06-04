cb56de3c006e4ec221babb74f889f6291119cf13
TakahiroT0415156 <teradatakahiro16@gmail.com>
2022-06-04 17:18:45 +0900

    topページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    collection do
+      get :top
+    end
+  end
 
-  root 'posts#index'
+  root 'posts#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
